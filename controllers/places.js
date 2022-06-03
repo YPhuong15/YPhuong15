4edefaaa3315b2dcf169adce7782cbcfdde2eb99
Yenphuong Tong <yenphuongtong@yenphuongs-mbp.lan>
2022-06-03 02:48:25 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,15 +1,6 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
-
-router.get('/', (req, res) => {  
-  res.render('places/index', {places})
-})
-
 router.post('/', (req, res) => {
   if (!req.body.pic) {
     // Default image if one is not provided
@@ -25,5 +16,25 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
+})
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
 
 module.exports = router
